46336f00603499bb7229385de0e971a645fa6510
Rishan98rasdeen <rishan25shehan@gmail.com>
2023-04-03 16:16:00 +0530

    Verilog files

---
+++ Sources/MUX_Control.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/29/2023 08:20:38 PM
+// Design Name: 
+// Module Name: MUX_Control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUX_Control(
+
+    );
+endmodule
